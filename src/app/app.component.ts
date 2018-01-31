a6bfa633532e99b99bdeb38420cc69d615ee4233
_bingbinghan_ <bingbinghan@aimsphm.com>
2018-01-31 13:53:34 +0800

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { NzMessageService } from '../../../index.showcase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  // isLoadingOne = false;
+
+  // loadOne = (value) => {
+  // 	this.isLoadingOne = true;
+  // 	setTimeout(_ => {
+  // 		this.isLoadingOne = false;
+  // 	}, 5000);
+  // }
+
+  // orderList = [1, 2, 3, 4]; 
+
+  ngOnInit() {
+  // 	setTimeout(_ => {
+  // 		this.orderList = [...this.orderList.reverse()];
+  // 	},10000)
+  }
+
+  // generateArray(value): void {
+  // 	return new Array(value);
+  // }
+
+  current = 0;
+  index = 'First-content';
+
+  pre(): void {
+  	this.current -= 1;
+  	this.changeContent();
+  }
+
+  next(): void {
+  	this.current += 1;
+  	this.changeContent();
+  }
+
+  done(): void {
+  	// this._message.success('done');
+  	alert('done');
+  }
+
+  changeContent() {
+  	switch (this.current) {
+  		case 0: {
+  			this.index = 'First-content';
+  			break;
+  		}
+  		case 1: {
+  		    this.index = 'Second-content';
+  		    break;
+  		}
+  		case 2: {
+  			this.index = 'third-content';
+  			break;
+  		}
+  		default: {
+  			this.index = 'error';
+  		}
+  	}
+  }
+
 }
